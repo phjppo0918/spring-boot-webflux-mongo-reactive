1098e275ba51dccc0dfdc7fb2b89c4e26fec5fe9
Hyeonjun Park <phjppo0918@gmail.com>
2023-05-03 16:55:04 +0900

    test : Member 생성 확인

---
+++ src/test/java/com/example/reactivemongo/domain/member/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.reactivemongo.domain.member;
+
+import com.example.reactivemongo.domain.member.dto.MemberRequest;
+import com.example.reactivemongo.domain.member.dto.MemberResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+@DisplayName("MemberService의")
+class MemberServiceTest {
+    @Autowired MemberService memberService;
+
+    @Nested
+    @DisplayName("create(MemberRequest) 에서")
+    class callCreate {
+        @Test
+        @DisplayName("생성을 수행하는가")
+        void successCreate() {
+            //given
+            MemberRequest dto = new MemberRequest("name", "password");
+            //when
+            Mono<MemberResponse> result = memberService.create(dto);
+            //then
+            StepVerifier.create(result)
+                    .expectNextMatches(m -> m.getName().equals(dto.getName()))
+                    .expectComplete()
+                    .verify();
+        }
+    }
+}
